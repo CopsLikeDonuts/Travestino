3097bd0bea6f0776a833a30b5987b1e04a14bde1
copslikedonuts <reggisen@gmail.com>
2020-12-27 23:54:04 +0200

    first commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+      <div className='content'>
+        <div>
+          <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0_IoZ1zN3m5kSH-vs3pd4otJhincC9ZuvsQ&usqp=CAU'></img>
+        </div>
+        <div>
+          ava + desc
+      </div>
+        <div>
+          my posts
+        <div>
+            new post
+        </div>
+          <div>
+            <div>
+              post 1
+          </div>
+            <div>
+              post 2
+          </div>
+
+          </div>
+        </div>
+      </div>
+    )
+}
+
+export default Profile;
