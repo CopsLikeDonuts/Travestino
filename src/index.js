02b2dbc89f13a6ea211c2f4a440b0f61e83985c6
Demian Shtepa <demyan.shtepa@gmail.com>
2021-01-23 15:44:30 +0200

    34

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import state from './redux/state'
-import {rerenderEntireTree} from './render';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import {addPost} from './redux/state';
 
+
+export let rerenderEntireTree = (state) => {
+  ReactDOM.render(
+    <App state={state} 
+         addPost={addPost}
+         updateNewPostText={updateNewPostText}/>,
+    document.getElementById('root')
+  );
+}
 rerenderEntireTree(state);
 
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,6 @@
-import {rerenderEntireTree} from '../render';
+let rerenderEntireTree = () => {
+    console.log('state has changed');
+}
 
 let state = {
     profilePage: {
