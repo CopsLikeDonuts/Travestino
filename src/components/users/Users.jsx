c186914420d8dbead4cd9446c24362ca2217eeb6
copslikedonuts <reggisen@gmail.com>
2021-02-23 16:40:17 +0200

    created users class

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -1,80 +1,49 @@
-import React from 'react';
+import axios from 'axios';
+import  React from 'react';
 import styles from './Users.module.css';
 
-const Users = (props) => {
+class Users extends React.Component{
 
-    if (props.users.length === 0) {
-        props.setUsers([
-            {id: 1,
-             followed: false, 
-             fullName: 'Dmitriy',
-             photoUrl: './userAvatar.png', 
-             status: 'I am a boss', 
-             location: {
-                city: 'Minsk',
-                country: 'Belarus'
-                } 
-            },
-            {id: 2,
-             followed: true, 
-             fullName: 'Sasha',
-             photoUrl: './userAvatar.png',  
-             status: 'I love porgramming', 
-             location: {
-                city: 'Kyiv',
-                country: 'Ukraine'
-                   } 
-               },
-            {id: 3,
-             followed: true, 
-             fullName: 'Petro',
-             photoUrl: './userAvatar.png',  
-             status: 'Status', 
-             location: {
-                city: 'Odessa',
-                country: 'Ukraine'
-                } 
-               },
-            {id: 4,
-             followed: false, 
-             fullName: 'John',
-             photoUrl: './userAvatar.png',  
-             status: 'Want to meet new friends', 
-             location: {
-                city: 'New-York',
-                country: 'USA'
-                   } 
-               },
-        ]);
+    getUsers = () => {
+        if (this.props.users.length === 0) {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users')
+                .then(response => {
+                    this.props.setUsers(response.data.items);
+                });
+        }
     }
 
-    
-    return <div>
-        {
-            props.users.map(u => <div key={u.id}>
-                <span>
-                    <div>
-                        <img className={styles.usersPhoto} src={u.photoUrl} alt='userAvatar'></img>
-                    </div>
-                    <div>
-                        { u.followed 
-                        ? <button onClick={() => {props.unfollow(u.id)}}>Unfollow</button>
-                        : <button onClick={() => {props.follow(u.id)}}>Follow</button>}
-                    </div>
-                </span>
-                <span>
-                    <span>
-                        <div>{u.fullName}</div>
-                        <div>{u.status}</div>
-                    </span>
-                    <span>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
-                    </span>
-                </span>
-            </div>)
-        }
-    </div>
+    render() {
+        return <div>
+                 <button onClick={this.getUsers}>Get users</button>
+                 {
+                    this.props.users.map(u => <div key={u.id}>
+                        <span>
+                            <div>
+                                <img className={styles.usersPhoto} 
+                                     src={u.photos.small != null ? u.photos.small: './userAvatar.png'} alt='userAvatar'></img>
+                            </div>
+                            <div>
+                                { u.followed 
+                                ? <button onClick={() => {this.props.unfollow(u.id)}}>Unfollow</button>
+                                : <button onClick={() => {this.props.follow(u.id)}}>Follow</button>}
+                            </div>
+                        </span>
+                        <span>
+                            <span>
+                                <div>{u.fullName}</div>
+                                <div>{u.status}</div>
+                            </span>
+                            <span>
+                                <div>{"u.location.country"}</div>
+                                <div>{"u.location.city"}</div>
+                            </span>
+                        </span>
+                    </div>)
+                }
+            </div>
+    }
 }
 
 export default Users;
+
